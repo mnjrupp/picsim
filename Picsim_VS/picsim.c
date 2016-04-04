181dec1d58fb9579cb92177e53bf2abe1714dbd2
mnjrupp <mnjrupp@hotmail.com>
2016-04-04 16:07:17 -0500

    VS 2013 version

---
+++ Picsim_VS/picsim.c
@@ -0,0 +1,280 @@
+/* ########################################################################
+
+   PICsim - PIC simulator
+
+   ########################################################################
+
+   Copyright (c) : 2008-2015  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#ifdef WIN32
+// necesssary to create own unisted.h header
+// and place in root of project files
+#include "unisted.h"
+#else
+#include<unistd.h>
+#endif
+#include"picsim.h"
+
+#ifdef WIN32
+#include<windows.h>
+#define usleep(x) Sleep(x)
+#else
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include "ansi.h"
+#endif
+  
+
+_pic pic1; 
+
+void exitcc(int sig) 
+{
+ pic_end(&pic1);
+ exit(-1);
+//void) signal(SIGINT, SIG_DFL);
+}
+
+int write_ihx(_pic * pic,const char * fname);
+int write_ihx18(_pic * pic,const char * fname);
+
+int 
+main(int argc,char** argv)
+{
+ 
+  int i;
+ 
+  char c=0;
+
+  unsigned int addr;
+
+  unsigned int brk; 
+  unsigned int nrep=0;
+  int rep=0;
+  int proc;
+  int family;
+  unsigned int fst=0;
+
+  if(argc == 3)
+  {
+
+#ifndef WIN32
+  (void) signal(SIGINT, exitcc);
+#endif
+ 
+    proc=getprocbyname(argv[1]);
+    if(proc == 0)
+    {
+        printf("Error: Invalid processor (%s)\n",argv[1]); 
+        return -1; 
+    }
+  
+    family=getfprocbyname(argv[1]);
+            
+    pic_set_serial(&pic1,"/dev/tnt2",0,0,0);
+    
+    pic_init(&pic1,family,proc,argv[2],1,20e6);
+
+   /* 
+   if(family == P16)
+     write_ihx(&pic1,"out.hex");
+   else
+    write_ihx18(&pic1,"out.hex");
+   */
+    
+    while(c != 'q')
+    {
+
+    //  printf("pc=%#06X w=%#04X 0x00=%#04X 0x04=%#04X 0x05=%#04X 0x06=%#04X status=%#04X intcon=%#04X\n",pic1.pc,pic1.w,pic1.ram[0x00],pic1.ram[0x04],pic1.ram[0x05],pic1.ram[0x06],pic1.ram[STATUS],pic1.ram[INTCON]);
+
+      do
+      {
+
+      if(fst)
+      {
+        pic_step(&pic1,0);
+      }
+      else
+      { 
+#ifndef WIN32      
+        ANSI_FG_HCOLOR(RED);
+#endif 
+        pic_step(&pic1,1);
+
+#ifndef WIN32      
+        ANSI_DEFAULT(); 
+#endif
+        
+        if(family == P16)
+        {
+        printf("PC=0x%04X W=\033[1;32m 0x%#02X \033[0m STATUS=0x%02X PORTA=0x%02X PORTB=0x%02X INTCON=\033[1;32m 0x%02X \033[0m TMR0=0x%02X TMR1H=0x%04X TMR1L=0x%04X TMR2=0x%04X\n",pic1.pc,pic1.w,
+         pic1.ram[STATUS],pic1.ram[PORTA],pic1.ram[PORTB],pic1.ram[INTCON],pic1.ram[TMR0],pic1.ram[TMR1H],  
+         pic1.ram[TMR1L],pic1.ram[TMR2]); 
+
+     for(i=0;i<16;i++)
+	printf("%#04X   ",pic1.ram[i]);
+     printf("\n");
+        }
+        else //P18
+        {
+           printf("PC    =0x%04X W=\033[1;32m 0x%02X \033[0m  STKPTR=0x%02X TOS=0x%06X PORTB=0x%02X PORTE=0x%02X status=\033[1;32m 0x%02X \033[0m INTCON=0x%02X afsr0=0x%04X afsr1=0x%04X afsr2=0x%04X\n",pic1.pc,pic1.w,
+  pic1.ram[P18_STKPTR],pic1.ram[P18_TOSU]<<16|pic1.ram[P18_TOSH]<<8|pic1.ram[P18_TOSL],pic1.ram[P18_PORTB],pic1.ram[P18_PORTE],pic1.ram[P18_STATUS],pic1.ram[P18_INTCON],  
+  ((pic1.ram[P18_FSR0H]&0x0F)<<8) |pic1.ram[P18_FSR0L],
+  ((pic1.ram[P18_FSR1H]&0x0F)<<8) |pic1.ram[P18_FSR1L],
+  ((pic1.ram[P18_FSR2H]&0x0F)<<8) |pic1.ram[P18_FSR2L]);
+       
+           printf("INTCON=0x%02X   PIE1=0x%02X PIR1=0x%02X   TMR1=0x%04X  CCP1=0x%04X\n",
+        pic1.ram[P18_INTCON],
+        pic1.ram[P18_PIE1],
+        pic1.ram[P18_PIR1],
+        (pic1.ram[P18_TMR1H]<<8)|pic1.ram[P18_TMR1L],
+        (pic1.ram[P18_CCPR1H]<<8)|pic1.ram[P18_CCPR1L]    
+        );
+            
+     printf("0x110: ");
+     for(i=0;i<16;i++)
+	printf("%#04X   ",pic1.ram[i+0x110]);
+     printf("\n");
+
+     printf("0x120: ");
+     for(i=0;i<16;i++)
+	printf("%#04X   ",pic1.ram[i+0x120]);
+     printf("\n");
+
+     printf("FSR0=%#04X  POSTINC0=%#04X\n",(pic1.ram[P18_FSR0H]<<8)+pic1.ram[P18_FSR0L],pic1.ram[P18_POSTINC0]);
+/*
+     printf("0x550: ");
+     for(i=0;i<16;i++)
+	printf("%#04X   ",pic1.ram[i+0x550]);
+     printf("\n");
+*/
+        
+        }
+        
+        
+     }
+    //  for(i=0;i<18;i++)
+    //    printf("%02i %i %i %i\n",i,pic_get_pin_dir(&pic1,i+1), pic_get_pin(&pic1,i+1), pic_get_pin_type(&pic1,i+1));
+      
+      if(!rep) 
+      { 
+        c=getchar();
+      }
+      else
+      {
+        if(pic1.pc == brk)
+        {
+         rep=0;
+         fst=0;
+         printf("\nBreakpoint ------------------------------------------------------------\n"); 
+        }
+        if(brk == 0)
+        { 
+          if(nrep == 0)
+            rep=0;
+          else
+            nrep--;
+        } 
+      };
+ 
+     //  usleep(1);
+     
+     }
+     while(rep);
+
+     
+       switch(c)
+       {
+       case 'b':
+         printf("Stop address:\n"); 
+         scanf("%x",&brk);
+         rep=1;  
+         break;
+       case 'f':
+         printf("Stop address:\n"); 
+         scanf("%x",&brk);
+         rep=1;   
+         fst=1;
+         break;
+       case 'n':
+         printf("Number of steps:\n"); 
+         scanf("%x",&nrep);
+         brk=0;
+         rep=1;  
+         break;
+       case 'r':
+         printf("Reset:\n"); 
+         pic_reset(&pic1,0);
+         rep=0;  
+         break;
+       case 'm' :
+         printf("Memory address:\n"); 
+         scanf("%x",&addr); 
+         getchar();
+         do
+         {
+           printf("\n");
+           for(i=-8;i<8;i++)
+             if(((addr+i) >= 0)&&( addr+i < pic1.RAMSIZE))  
+	     printf("%#06X:  %#06X (%i)\n",addr+i,pic1.ram[addr+i],pic1.ram[addr+i]);
+          c=getchar();
+          getchar();
+          addr+=16;
+         }while(c == 'm');   
+         break;
+       case 'h': 
+         printf("commands:\n");
+         printf("  b - goto breakpoint\n");
+         printf("  f - goto breakpoint without print (fast)\n");
+         printf("  n - run n steps\n");
+         printf("  m - show memory\n");
+         printf("  r - reset\n");
+         printf("  q - quit\n");
+         break;
+      }
+       
+      if(c != 'q') c=0;
+     
+  
+
+    };
+
+     pic_end(&pic1);
+   }
+   else
+   {
+     printf("use: %s PICXXFXXX file.hex\n",argv[0]);
+     printf("supported processors PIC16:\n");
+     printf("         PIC16F628 PIC16F628A PIC16F877 PIC16F877A PIC16F648A PIC16F777\n");     
+     printf("supported processors PIC18:\n");
+     printf("         PIC18F452 PIC18F4620 PIC18F4550\n");
+
+#ifdef WIN32
+    system("PAUSE");
+#endif        
+   }
+    return 1;
+}
+
+
---
+++ Picsim_VS/serial.c
@@ -0,0 +1,740 @@
+/* ########################################################################
+
+   PICsim - PIC simulator
+
+   ########################################################################
+
+   Copyright (c) : 2008-2015  Luis Claudio Gambôa Lopes
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   For e-mail suggestions :  lcgamboa@yahoo.com
+   ######################################################################## */
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#ifdef WIN32
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+#else
+#include <termios.h>
+#include <sys/ioctl.h>
+#endif
+
+#include "picsim.h"
+#include "periferic.h"
+#include "periferic18.h"
+        
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+        
+
+
+int 
+serial_open(_pic * pic)
+{
+      if(pic->SERIALDEVICE[0] == 0)
+      { 	
+#ifdef WIN32
+        strcpy(pic->SERIALDEVICE,"COM2");
+#else
+        strcpy(pic->SERIALDEVICE,"/dev/tnt2");
+#endif
+      }
+
+  pic->bc=0;
+  pic->sr=0;
+  pic->serialc=0;
+  pic->recb=0;
+  pic->s_open=0;
+
+#ifdef WIN32
+  pic->serialfd = CreateFile(pic->SERIALDEVICE, GENERIC_READ | GENERIC_WRITE,
+0, // exclusive access
+NULL, // no security
+OPEN_EXISTING,
+0, // no overlapped I/O
+NULL); // null template
+  if( pic->serialfd == INVALID_HANDLE_VALUE)
+  {
+     pic->serialfd=0;
+//     printf("Erro on Port Open:%s!\n",pic->SERIALDEVICE);
+     return 0; 
+  }
+#else
+  pic->serialfd = open(pic->SERIALDEVICE, O_RDWR | O_NOCTTY | O_NONBLOCK);
+ 
+  if (pic->serialfd < 0) 
+  {
+    pic->serialfd=0;
+    perror(pic->SERIALDEVICE); 
+//    printf("Erro on Port Open:%s!\n",pic->SERIALDEVICE);
+    return 0; 
+  }
+//  printf("Port Open:%s!\n",pic->SERIALDEVICE);
+#endif
+  return 1;
+}
+
+int 
+serial_close(_pic * pic)
+{
+  if (pic->serialfd != 0) 
+  {
+#ifdef WIN32
+  CloseHandle(pic->serialfd);
+#else    
+    close(pic->serialfd);
+#endif
+    pic->serialfd=0;
+  }
+  return 0;
+}
+
+
+
+int
+serial_cfg(_pic * pic)
+{
+    unsigned int BAUDRATE;
+   
+    if(pic->family == P16)
+    { 
+      if(pic->ram[TXSTA] & 0x04) //BRGH=1 
+      {
+         pic->serialexbaud=pic->freq/(16*(pic->ram[SPBRG]+1));
+      }
+      else
+      {
+        pic->serialexbaud=pic->freq/(64*(pic->ram[SPBRG]+1));
+      }
+    }
+    else
+    {
+      if(pic->ram[P18_TXSTA] & 0x04) //BRGH=1 
+      {
+         pic->serialexbaud=pic->freq/(16*(pic->ram[P18_SPBRG]+1));
+      }
+      else
+      {
+        pic->serialexbaud=pic->freq/(64*(pic->ram[P18_SPBRG]+1));
+      }
+
+    }
+
+
+
+    switch(((int)((pic->serialexbaud/300.0)+0.5))) 
+    {
+       case 0 ... 1:
+          pic->serialbaud=300;
+          #ifndef WIN32
+          BAUDRATE=B300;
+          #else
+          BAUDRATE=300;
+          #endif  
+          break; 
+       case 2 ... 3:
+          pic->serialbaud=600;
+          #ifndef WIN32
+          BAUDRATE=B600;
+          #else
+          BAUDRATE=600;
+          #endif  
+          break; 
+       case 4 ... 7:
+          pic->serialbaud=1200;
+          #ifndef WIN32
+          BAUDRATE=B1200;
+          #else
+          BAUDRATE=1200;
+          #endif  
+          break; 
+       case 8 ... 15:
+          pic->serialbaud=2400;
+          #ifndef WIN32
+          BAUDRATE=B2400;
+          #else
+          BAUDRATE=2400;
+          #endif  
+          break; 
+       case 16 ... 31:
+          pic->serialbaud=4800;
+          #ifndef WIN32
+          BAUDRATE=B4800;
+          #else
+          BAUDRATE=4800;
+          #endif  
+          break; 
+       case 32 ... 63:
+          pic->serialbaud=9600;
+          #ifndef WIN32
+          BAUDRATE=B9600;
+          #else
+          BAUDRATE=9600;
+          #endif  
+          break; 
+       case 64 ... 127:
+          pic->serialbaud=19200;
+          #ifndef WIN32
+          BAUDRATE=B19200;
+          #else
+          BAUDRATE=19200;
+          #endif  
+          break; 
+       case 128 ... 191:
+          pic->serialbaud=38400;
+          #ifndef WIN32
+          BAUDRATE=B38400;
+          #else
+          BAUDRATE=38400;
+          #endif  
+          break; 
+       case 192 ... 383:
+          pic->serialbaud=57600;
+          #ifndef WIN32
+          BAUDRATE=B57600;
+          #else
+          BAUDRATE=57600;
+          #endif  
+          break; 
+       default:
+          pic->serialbaud=115200;
+          #ifndef WIN32
+          BAUDRATE=B115200;
+          #else
+          BAUDRATE=115200;
+          #endif  
+          break; 
+    } 
+
+#ifdef WIN32
+  BOOL bPortReady;
+  DCB dcb;
+  COMMTIMEOUTS CommTimeouts;
+
+bPortReady = GetCommState(pic->serialfd , &dcb);
+dcb.BaudRate = BAUDRATE;
+dcb.ByteSize = 8;
+dcb.Parity = NOPARITY;
+dcb.StopBits = ONESTOPBIT;
+dcb.fAbortOnError = TRUE;
+
+// set XON/XOFF
+dcb.fOutX = FALSE; // XON/XOFF off for transmit
+dcb.fInX = FALSE; // XON/XOFF off for receive
+// set RTSCTS
+dcb.fOutxCtsFlow = FALSE; // turn off CTS flow control
+//dcb.fRtsControl = RTS_CONTROL_HANDSHAKE; //
+dcb.fRtsControl = RTS_CONTROL_DISABLE; //
+// set DSRDTR
+dcb.fOutxDsrFlow = FALSE; // turn off DSR flow control
+//dcb.fDtrControl = DTR_CONTROL_ENABLE; //
+dcb.fDtrControl = DTR_CONTROL_DISABLE; //
+// dcb.fDtrControl = DTR_CONTROL_HANDSHAKE; //
+
+bPortReady = SetCommState(pic->serialfd , &dcb);
+
+// Communication timeouts are optional
+
+bPortReady = GetCommTimeouts (pic->serialfd , &CommTimeouts);
+
+CommTimeouts.ReadIntervalTimeout = MAXDWORD;
+CommTimeouts.ReadTotalTimeoutConstant = 0;
+CommTimeouts.ReadTotalTimeoutMultiplier = 0;
+CommTimeouts.WriteTotalTimeoutConstant = 0;
+CommTimeouts.WriteTotalTimeoutMultiplier = 0;
+
+bPortReady = SetCommTimeouts (pic->serialfd , &CommTimeouts);
+	
+
+EscapeCommFunction(pic->serialfd ,SETRTS );
+
+#else
+   struct termios newtio;
+   int cmd;   
+        
+//        tcgetattr(fd,&oldtio); /* save current port settings */
+        
+        bzero(&newtio, sizeof(newtio));
+        newtio.c_cflag = BAUDRATE |CS8 | CLOCAL | CREAD;
+        newtio.c_iflag = IGNPAR|IGNBRK;
+        newtio.c_oflag = 0;
+        
+        /* set input mode (non-canonical, no echo,...) */
+        newtio.c_lflag = 0;
+         
+        newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+        newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+        
+        tcflush(pic->serialfd, TCIFLUSH);
+        tcsetattr(pic->serialfd,TCSANOW,&newtio);
+        
+	cmd=TIOCM_RTS;
+	ioctl(pic->serialfd, TIOCMBIS ,&cmd);
+#endif
+
+	return 0; 
+}
+
+
+      
+unsigned long serial_send(_pic * pic, unsigned char c)
+{
+  if(pic->serialfd)
+  {
+#ifdef WIN32
+   unsigned long nbytes;
+    
+   WriteFile(pic->serialfd, &c, 1, &nbytes,NULL);
+   return nbytes;
+#else
+  return write (pic->serialfd,&c,1);   
+#endif
+  }
+  else
+    return 0;
+}
+
+unsigned long serial_rec(_pic * pic, unsigned char * c)
+{
+  if(pic->serialfd)
+  {
+#ifdef WIN32
+    unsigned long nbytes;
+      
+    ReadFile(pic->serialfd, c, 1,&nbytes, NULL);
+#else
+    long nbytes;
+
+     nbytes = read (pic->serialfd,c,1);   
+     if(nbytes<0)nbytes=0;
+#endif    
+    return nbytes;
+   }
+   else
+     return 0;
+}
+
+unsigned long serial_rec_tout(_pic * pic, unsigned char * c)
+{
+ unsigned int tout=0;
+
+  if(pic->serialfd)
+  {
+#ifdef WIN32
+    unsigned long nbytes;
+    do
+    { 
+      Sleep(1);	
+      ReadFile(pic->serialfd, c, 1,&nbytes, NULL);
+#else
+    long nbytes;
+    do
+    { 
+      usleep(100);
+      nbytes = read (pic->serialfd,c,1);   
+      if(nbytes<0)nbytes=0;
+#endif    
+      tout++;
+    }while((nbytes == 0 )&&(tout < 1000));
+    return nbytes;
+   }
+   else
+     return 0;
+}
+
+
+unsigned long serial_recbuff(_pic * pic, unsigned char * c)
+{
+int i;
+
+
+  if(pic->flowcontrol)
+  {
+  
+   if(serial_rec(pic,&pic->buff[pic->bc]) == 1)
+    {
+     pic->bc++;
+
+     if(pic->bc > BUFFMAX)
+     {
+       printf("serial buffer overflow \n") ;
+       pic->bc = BUFFMAX-1;  
+//       getchar();	
+     };
+    }
+
+
+    if((pic_get_pin(pic,pic->ctspin) == 0)&&(pic->bc > 0))
+    {
+      *c=pic->buff[0];
+
+      pic->bc--;
+      for(i=0;i<pic->bc;i++)
+        pic->buff[i]=pic->buff[i+1]; 
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  else
+  {
+     return serial_rec(pic,c);
+  }
+  
+}
+
+
+
+
+
+void serial(_pic * pic,int print)
+{
+  unsigned char rctemp;
+
+
+  if((pic->ram[RCSTA] & 0x80)==0x80)
+  {
+    if(pic->s_open == 0) 
+    {
+
+      if(pic->serialfd > 0)
+      {
+        serial_cfg(pic);
+        pic->s_open=1;
+       if(print)printf("#Open Port:%s!\n",pic->SERIALDEVICE);
+      }
+      else
+      {
+        if(print)printf("#Erro Open Port:%s!\n",pic->SERIALDEVICE);
+        pic->s_open=-1;
+      }
+      pic->ram[TXSTA]|=0x02; //TRMT=1 empty 
+      pic->ram[PIR1]|=0x10; //TXIF=1  
+    }
+   pic->pins[pic->usart[0]-1].ptype=PT_USART;
+   pic->pins[pic->usart[1]-1].ptype=PT_USART;
+   
+   if(pic->flowcontrol)pic_set_pin(pic, pic->rtspin,0); //enable send
+  }
+  else
+  {
+    if(pic->s_open == 1)
+    {
+      pic->s_open=0;
+    }
+   pic->pins[pic->usart[0]-1].ptype=PT_CMOS;
+   pic->pins[pic->usart[1]-1].ptype=PT_CMOS;
+   if(pic->flowcontrol)pic_set_pin(pic, pic->rtspin,1); //disable send
+  }
+  
+
+   if(pic->lram == TXREG)    
+    { 
+      pic->txtemp=pic->ram[TXREG];
+      pic->ram[TXSTA]&=~0x02; //TRMT=0 full   
+      pic->ram[PIR1]&=~0x10; //TXIF=0 trasmiting 
+    }
+   
+
+   if(pic->lram == RCSTA)
+   {               //CREN 
+      if((pic->ram[RCSTA] & 0x10) == 0)
+      {  
+       pic->ram[RCSTA]&=~0x02; //clear OERR
+       pic->serialc=0;
+      }
+   }    
+
+
+   if(pic->rram == RCREG)    
+    { 
+      switch(pic->recb)
+      {
+        case 1:
+          pic->ram[RCREG]=0;
+          pic->ram[PIR1]&=~0x20; //clear RCIF
+          pic->recb--;    
+          break;
+        case 2:
+          pic->ram[RCREG]=pic->RCREG2;
+          pic->RCREG2=0; 
+	  pic->recb--;    
+          break;
+        default:
+        break; 
+      }
+
+    }
+
+
+    pic->serialc++;
+
+    if((pic->ram[TXSTA] & 0x04) == 0x04)
+    {
+       //BRGH=1  start + 8 bits + stop
+       if(pic->serialc >= ((pic->ram[SPBRG]+1)*40))pic->sr =1;
+    }
+    else
+    {
+       //BRGH=0  start + 8 bits + stop
+       if(pic->serialc >= ((pic->ram[SPBRG]+1)*160))pic->sr =1;
+    }
+
+  
+    if(pic->sr ==1 )
+    {
+    
+     pic->serialc=0;
+     
+     
+     
+     if((pic->s_open != 0)&&((pic->ram[pic->pins[pic->usart[0]-1].port+0x80] &  (0x01 << pic->pins[pic->usart[0]-1].pord)) != 0))//work only if RX TRIS is set
+     {
+      if(serial_recbuff(pic,&rctemp) == 1)
+      {
+
+        if((pic->ram[RCSTA] & 0x12) == 0x10)//CREN=1  OERR=0 
+        { 
+         switch(pic->recb)
+         {
+         case 0: 
+           pic->ram[RCREG]=rctemp;
+	   pic->RCREG2=0;
+           pic->recb++;
+           break;
+         case 1: 
+           pic->RCREG2=rctemp;
+           pic->recb++;
+           break; 
+         default: 
+           pic->ram[RCSTA]|=0x02; //set OERR
+           break; 
+          }
+        }
+        
+   //       printf("reb=%i temp=%02X RCREG=%02X RECREG2=%02X  RCSTA=%02X\n",pic->recb,rctemp,pic->ram[RCREG],pic->RCREG2,pic->ram[RCSTA]);
+        
+         if(((pic->ram[PIE1] & 0x20) == 0x20)&&((pic->ram[PIR1] & 0x20) != 0x20))
+         {
+           if(print)printf("serial rx interrupt (%#04X)\n",rctemp);
+         }
+         //set RCIF
+         pic->ram[PIR1]|=0x20;  
+      }
+     } 
+    
+      //if(((pic->ram[TXSTA] & 0x02) == 0 )&&((pic->ram[pic->pins[pic->usart[1]-1].port+0x80] &  (0x01 << pic->pins[pic->usart[1]-1].pord)) != 0))
+      if((pic->ram[TXSTA] & 0x02) == 0 )
+      {   
+        if(pic->s_open == 1 ) serial_send(pic,pic->txtemp);
+        pic->ram[TXSTA]|=0x02; //TRMT=1 empty  
+    
+        if(((pic->ram[PIE1] & 0x10) == 0x10)&&((pic->ram[PIR1] & 0x10) != 0x10))
+        {
+          if(print)printf("serial tx interrupt (%#04X)\n",pic->txtemp);
+        }
+        pic->ram[PIR1]|=0x10; //TXIF=1  
+      }   
+      pic->sr=0;
+    }
+
+//Hardware flowcontrol
+
+
+
+}
+
+void serial18(_pic * pic,int print)
+{
+  unsigned char rctemp;
+
+
+  if((pic->ram[P18_RCSTA] & 0x80)==0x80)
+  {
+    if(pic->s_open == 0) 
+    {
+
+      if(pic->serialfd > 0)
+      {
+        serial_cfg(pic);
+        pic->s_open=1;
+       if(print)printf("#Open Port:%s!\n",pic->SERIALDEVICE);
+      }
+      else
+      {
+        if(print)printf("#Erro Open Port:%s!\n",pic->SERIALDEVICE);
+        pic->s_open=-1;
+      }
+      pic->ram[P18_TXSTA]|=0x02; //TRMT=1 empty 
+      pic->ram[P18_PIR1]|=0x10; //TXIF=1  
+    }
+   pic->pins[pic->usart[0]-1].ptype=PT_USART;
+   pic->pins[pic->usart[1]-1].ptype=PT_USART;
+   
+   if(pic->flowcontrol)pic_set_pin(pic, pic->rtspin,0); //enable send
+  }
+  else
+  {
+    if(pic->s_open == 1)
+    {
+      pic->s_open=0;
+    }
+   pic->pins[pic->usart[0]-1].ptype=PT_CMOS;
+   pic->pins[pic->usart[1]-1].ptype=PT_CMOS;
+   if(pic->flowcontrol)pic_set_pin(pic, pic->rtspin,1); //disable send
+  }
+  
+
+   if(pic->lram == P18_TXREG)    
+    { 
+      pic->txtemp=pic->ram[P18_TXREG];
+      pic->ram[P18_TXSTA]&=~0x02; //TRMT=0 full   
+      pic->ram[P18_PIR1]&=~0x10; //TXIF=0 trasmiting 
+    }
+   
+
+   if(pic->lram == P18_RCSTA)
+   {               //CREN 
+      if((pic->ram[P18_RCSTA] & 0x10) == 0)
+      {  
+       pic->ram[P18_RCSTA]&=~0x02; //clear OERR
+       pic->serialc=0;
+      }
+   }    
+
+
+   if(pic->rram == P18_RCREG)    
+    { 
+      switch(pic->recb)
+      {
+        case 1:
+          pic->ram[P18_RCREG]=0;
+          pic->ram[P18_PIR1]&=~0x20; //clear RCIF
+          pic->recb--;    
+          break;
+        case 2:
+          pic->ram[P18_RCREG]=pic->RCREG2;
+          pic->RCREG2=0; 
+	  pic->recb--;    
+          break;
+        default:
+        break; 
+      }
+
+    }
+
+
+    pic->serialc++;
+
+    if(pic->ram[P18_TXSTA] & 0x04)
+    {
+       //BRGH=1  start + 8 bits + stop
+       if(pic->serialc >= ((pic->ram[P18_SPBRG]+1)*40))pic->sr =1;
+    }
+    else
+    {
+       //BRGH=0  start + 8 bits + stop
+       if(pic->serialc >= ((pic->ram[P18_SPBRG]+1)*160))pic->sr =1;
+    }
+
+  
+    if(pic->sr ==1 )
+    {
+    
+     pic->serialc=0;
+  
+     if((pic->s_open != 0)&&((pic->ram[pic->pins[pic->usart[0]-1].port+0x12] &  (0x01 << pic->pins[pic->usart[0]-1].pord)) != 0)) //work only if RX tris bit is set
+     {
+      if(serial_recbuff(pic,&rctemp) == 1)
+      {
+
+        if((pic->ram[P18_RCSTA] & 0x12) == 0x10)//CREN=1  OERR=0 
+        { 
+         switch(pic->recb)
+         {
+         case 0: 
+           pic->ram[P18_RCREG]=rctemp;
+	   pic->RCREG2=0;
+           pic->recb++;
+           break;
+         case 1: 
+           pic->RCREG2=rctemp;
+           pic->recb++;
+           break; 
+         default: 
+           pic->ram[P18_RCSTA]|=0x02; //set OERR
+           break; 
+          }
+        }
+        
+   //       printf("reb=%i temp=%02X RCREG=%02X RECREG2=%02X  RCSTA=%02X\n",pic->recb,rctemp,pic->ram[RCREG],pic->RCREG2,pic->ram[RCSTA]);
+        
+         if(((pic->ram[P18_PIE1] & 0x20) == 0x20)&&((pic->ram[P18_PIR1] & 0x20) != 0x20))
+         {
+           if(print)printf("serial rx interrupt (%#04X)\n",rctemp);
+         }
+         //set RCIF
+         pic->ram[P18_PIR1]|=0x20;  
+      }
+     } 
+    
+      //if(((pic->ram[P18_TXSTA] & 0x02) == 0 ) &&((pic->ram[pic->pins[pic->usart[1]-1].port+0x12] &  (0x01 << pic->pins[pic->usart[1]-1].pord)) == 0))
+      if((pic->ram[P18_TXSTA] & 0x02) == 0 )
+       {   
+        if(pic->s_open == 1 ) serial_send(pic,pic->txtemp);
+        pic->ram[P18_TXSTA]|=0x02; //TRMT=1 empty  
+    
+        if(((pic->ram[P18_PIE1] & 0x10) == 0x10)&&((pic->ram[P18_PIR1] & 0x10) != 0x10))
+        {
+          if(print)printf("serial tx interrupt (%#04X)\n",pic->txtemp);
+        }
+        pic->ram[P18_PIR1]|=0x10; //TXIF=1  
+      }   
+      pic->sr=0;
+    }
+
+//Hardware flowcontrol
+
+
+
+}
+
+
+void 
+pic_set_serial(_pic * pic, char * name, int flowcontrol,int ctspin,int  rtspin)
+{
+  strcpy(pic->SERIALDEVICE,name);
+  
+  pic->flowcontrol=flowcontrol;
+  pic->serialfd=0;
+  if(flowcontrol == 1)
+  {
+    pic->ctspin=ctspin;
+    pic->rtspin=rtspin;
+  }
+};
